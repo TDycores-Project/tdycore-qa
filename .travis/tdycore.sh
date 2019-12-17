369b564b5f3bb0c27a672177c16c277234c57410
Rosie <rleone@sandia.gov>
2019-12-17 12:53:42 -0700

    testing

--- .travis/tdycore.sh
+++ .travis/tdycore.sh
@@ -84,11 +84,12 @@ git clone https://github.com/leorosie/TDycore-test.git
 ls -a
 echo "reg folder"
 
+var=$(pwd)
 #insert simulator paths into simulators.sim
 echo '[simulators]
 python = /usr/bin/python3
-tdycore = $PWD/TDycore/demo/steadyblock/steadyblock
-pflotran = $PWD/pflotran/src/pflotran/pflotran' >$PWD/qa-toolbox/simulators.sim
+tdycore =' $var'/TDycore/demo/steadyblock/steadyblock
+pflotran =' $var'/pflotran/src/pflotran/pflotran' >$PWD/qa-toolbox/simulators.sim
 
 ##map tdycore test within config_files.txt
 echo '../TDycore-test/2d_block/2d_block.cfg'>$PWD/qa-toolbox/config_files.txt
